19b9fe75adfd96ff99459c0715285442da2fce57
ampinkus <ampinkus@gmail.com>
2024-12-07 15:55:20 -0300

    agregada página de compras

--- script/main.js
+++
@@ -1,4 +0,0 @@
-// Código JavaScript para mostrar y ocultar el menú en pantallas pequeñas
-document.querySelector(".menu-toggle").addEventListener("click", () => {
-  document.querySelector(".menu-links").classList.toggle("show");
-});
--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
-window.myFunction = function () {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+function actualizarCantidad(producto) {
+  const cantidad = document.getElementById(producto).value;
+  if (cantidad !== "") {
+    localStorage.setItem(producto, cantidad);
+    alert(`Se actualizó la cantidad de ${producto} a ${cantidad}`);
   } else {
-    x.className = "topnav";
+    alert("Por favor, ingresa una cantidad válida.");
   }
-};
+}
